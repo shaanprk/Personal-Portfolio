769e1a472ba09289a842e87a7a6a07a288c780ed
Sean <jungbeanshaanprk@gmail.com>
2024-09-15 19:59:05 +0900

    Navbar design changes

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,26 +1,23 @@
 <template>
     <header class="bg-gray-800 p-4">
-        <div class="container mx-auto flex justify-between items-center">
+        <div class="container flex items-center m-1">
             <router-link to="/"><img src="../assets/Cropped_Image.png" class="navbar-logo"/></router-link>
 
-            <button @click="toggleButton = !toggleButton">
-                <p :class="toggleButton ? 'hidden' : ''">hehe</p>
-                <p :class="toggleButton ? '' : 'hidden'">haha</p>
+            <button @click="toggleButton = !toggleButton" class="hover:tracking-wider mx-8 text-2xl text-white">
+                <p :class="toggleButton ? 'hidden' : ''">Menu</p>
+                <p :class="toggleButton ? '' : 'hidden'">Close</p>
             </button>
 
-
-
-
-
-
-            <nav class="navbar">
-                <ul class="flex space-x-4 text-white w-full justify-between">
-                    <li class="navbar-links"><router-link to="/">Home</router-link></li>
-                    <li class="navbar-links"><router-link to="/about">About</router-link></li>
-                    <li class="navbar-links"><router-link to="/projects">Projects</router-link></li>
-                    <li class="navbar-links"><router-link to="/contact">Contact</router-link></li>
-                </ul>
-            </nav>
+            <div id="menu">
+                <nav class="navbar">
+                    <ul class="flex space-x-4 text-white w-full justify-between">
+                        <li class="navbar-links"><router-link to="/">Home</router-link></li>
+                        <li class="navbar-links"><router-link to="/about">About</router-link></li>
+                        <li class="navbar-links"><router-link to="/projects">Projects</router-link></li>
+                        <li class="navbar-links"><router-link to="/contact">Contact</router-link></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </header>
 </template>
@@ -43,5 +40,8 @@
     }
     .navbar-links {
         @apply hover:text-gray-400 flex-grow text-center outline;
+    }
+    .navbar-hover {
+        @apply hover:tracking-wider;
     }
 </style>
